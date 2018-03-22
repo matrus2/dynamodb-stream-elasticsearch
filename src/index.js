6ac174e3236b7532e07403f7279a4d6139bb72d6
Bogusz Przybyslawski <biuro@matrus.pl>
2018-03-22 12:21:04 +0100

    Validate input params

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const elasticsearch = require('elasticsearch')
 const httpAwsEs = require('http-aws-es')
 
-exports.pushStream = async ({ index, type, id, body, endpoint, refresh = true, testMode = false } = {}) => {
+exports.pushStream = async ({index, type, id, body, endpoint, refresh = true, testMode = false} = {}) => {
+  // Data validation
+  if (!index || !(typeof index === 'string')) throw new Error('Please provide correct index')
+  if (!type || !(typeof type === 'string')) throw new Error('Please provide correct type')
+  if (!endpoint || !(typeof endpoint === 'string')) throw new Error('Please provide correct endpoint')
+
   /* CREATE THE CONNECTION TO ES */
-  let connParams = { hosts: endpoint }
+  let connParams = {hosts: endpoint}
   // AWS Elasticsearch requires special type of connection, in CI we will use plain elasticsearch container
   if (!testMode) connParams.connectionClass = httpAwsEs
   const es = new elasticsearch.Client(connParams)
 
-  es.index({ index, type, id, body, refresh }, (error, response) => {
+  es.index({index, type, id, body, refresh}, (error, response) => {
     if (error) console.log(error)
     console.log(response)
   })
--- test/modify.js
+++ test/modify.js
@@ -3,6 +3,17 @@ const { assert } = require('chai')
 const fetch = require('node-fetch')
 const { pushStream } = require('./../src/index')
 
+async function assertThrowsAsync (fn, regExp) {
+  let f = () => {}
+  try {
+    await fn()
+  } catch (e) {
+    f = () => { throw e }
+  } finally {
+    assert.throws(f, regExp)
+  }
+}
+
 const ES_ENDPOINT = 'localhost:9200'
 // 1. run a docker container
 // docker run -i -p 9200:9200 --name my_elastic -p 9300:9300 -e "discovery.type=single-node" elasticsearch
@@ -13,5 +24,17 @@ describe('MODIFY event', () => {
     const result = await fetch(`http://${ES_ENDPOINT}/${simpleData.index}/${simpleData.type}/${simpleData.id}`)
     const body = await result.json()
     assert.deepEqual(simpleData.body, body._source)
+  })
+  it('wrong input index is not a string', async () => {
+    const simpleData = { index: 12, type: 'test1', body: {test: 'test', myvalue: {1: 'test'}}, id: '12', endpoint: ES_ENDPOINT, testMode: true }
+    await assertThrowsAsync(async () => pushStream(simpleData), 'Please provide correct index')
+  })
+  it('wrong input type is not a string', async () => {
+    const simpleData = { index: 'asd', type: '', body: {test: 'test', myvalue: {1: 'test'}}, id: '12', endpoint: ES_ENDPOINT, testMode: true }
+    await assertThrowsAsync(async () => pushStream(simpleData), 'Please provide correct type')
+  })
+  it('wrong input endpoint is not a string', async () => {
+    const simpleData = { index: '12', type: 'test1', body: {test: 'test', myvalue: {1: 'test'}}, id: '12', testMode: true }
+    await assertThrowsAsync(async () => pushStream(simpleData), 'Please provide correct endpoint')
   })
 })
